f3754f4918a7d448314f43d20544ba3f289943c8
Kawan <kawang870@gmail.com>
2023-10-06 16:44:03 -0300

    feat: vendo algumas estruturas

---
+++ urnaEletronica.js
@@ -0,0 +1,59 @@
+// Estrutura: switch-case
+
+//const opcao =1
+
+//switch (opcao) {
+
+//    case 1:
+//        console.log('Opção é 1');
+//        break;
+//    case 2:
+//        console.log('Opção é 2');
+//        break;
+//   default:
+//        console.log('Nenhuma das opções')
+//        break;
+// }
+
+//if (opcao 1 ===) {
+//    console.log('Opção é 1') 
+//} else if (opcao 2 ===) {
+//    console.log('Opção é 2')
+//} else {
+//    console.log('Nenhuma das opções')
+//}
+
+// Estrutura de repetição: do-while
+
+// const contador = 1;
+
+// do {
+//     console.log('Instrução 1');
+//     console.log('Instrução 2');
+//     console.log('Instrução 3');
+//     console.log('Instrução 4');
+//     console.log('Instrução 5');
+//     console.log('Instrução 6');
+// } while (contador === 1);
+
+// Estrutura de repetição: while (enquanto)
+
+//const opcao = 0;
+
+//while (opcao !== 0) {
+    
+//    console.log('Instrução 1');
+//    console.log('Instrução 2');
+//    console.log('Instrução 3');
+//    console.log('Instrução 4');
+//    console.log('Instrução 5');
+//    console.log('Instrução 6');
+//}
+
+// Estrutura de repetição: while (enquanto)
+
+// const contador = 0;
+// do {
+//     console.log('Instrução');
+//     contador += 1;
+// } while (contador <= 10)
