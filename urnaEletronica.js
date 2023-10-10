5aaf1756d114e39016825973c4c70900a64ac008
Kawan <kawang870@gmail.com>
2023-10-10 16:46:45 -0300

    feat: sla

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -57,3 +57,21 @@
 //     console.log('Instrução');
 //     contador += 1;
 // } while (contador <= 10)
+
+function urnaEletronica() {
+
+    //Aqui vai todo o código do programa....
+
+    console.log('Iniciando o programa');
+
+    let
+        totalVotosCandidato1 = 0,
+        totalVotosCandidato2 = 0,
+        totalVotosCandidato3 = 0,
+        totalVotosBranco = 0,
+        totalVotosNulo = 0;
+    do {
+
+        //Instruções
+    } while (condicao);
+}
