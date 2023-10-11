ae71cda9d0bdd174e65e179d3c13ad83bcd0c1e5
Kawan <kawang870@gmail.com>
2023-10-11 14:15:30 -0300

    feat: finalizando o algoritmo da urna

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -57,21 +57,73 @@
 //     console.log('Instrução');
 //     contador += 1;
 // } while (contador <= 10)
-
 function urnaEletronica() {
 
-    //Aqui vai todo o código do programa....
+    let opcao;
+    let nome1;
+    let nome2;
+    let nome3;
 
-    console.log('Iniciando o programa');
+    let contador = 0;
+    let candidato1 = 0;
+    let candidato2 = 0;
+    let candidato3 = 0;
+    let votobranco = 0;
+    let votonulo = 0;
+    
 
-    let
-        totalVotosCandidato1 = 0,
-        totalVotosCandidato2 = 0,
-        totalVotosCandidato3 = 0,
-        totalVotosBranco = 0,
-        totalVotosNulo = 0;
     do {
+        
+        voto = parseInt(prompt(
+            '\n 1 Candidato 1' +
+            '\n 2 Candidato 2' +
+            '\n 3 Candidato 3' +
+            '\n 5 Voto em Branco' +
+            '\n 8 Voto Nulo '+
+            '\n 0 Encerrar votação' +
+            '\n Digite a sua escolha:'));
 
-        //Instruções
-    } while (condicao);
+        contador++;
+       
+        
+        if (voto === 1){
+    
+            console.log('Voto computado para:', nome1)
+            candidato1++;
+            
+        }else if (voto === 2){
+    
+            console.log('Voto computado para:', nome2)
+            candidato2++;
+    
+        }else if (voto === 3){
+    
+            console.log('Voto computado para:', nome3)
+            candidato3++;
+    
+        }else if (voto === 5){
+    
+            console.log('Voto em Branco computado')
+            votobranco++;
+    
+        }else if (voto === 8){
+    
+            console.log('Voto Nulo computado')
+            votonulo++;
+    
+        }
+
+    } while (voto !== 0);
+    
+    contador--;
+    
+
+    console.log('Contagem', contador );
+    console.log('Voto computado para Candidato1 ', candidato1);
+    console.log('Voto computado para Candidato2 ', candidato2);
+    console.log('Voto computado para Candidato3 ', candidato3);
+    console.log('Voto em Branco computado', votobranco);
+    console.log('Voto Nulo computado', votonulo);
+
+
 }
