920e32169fe5f8eda424b8f216e5a295dbd90dc1
Kawan <kawang870@gmail.com>
2023-10-19 14:14:53 -0300

    Desafio #0 resolvido

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -58,86 +58,93 @@
 //     contador += 1;
 // } while (contador <= 10)
 
-function urnaEletronica(){
-    let 
-    candidato1 = "",
-    candidato2 = "",
-    candidato3 = "",
-    fim = "",
-    ganhador = "";
+function urnaEletronica() {
 
-    let 
-    votoC1 = 0,
-    votoC2 = 0,
-    votoC3 = 0,
-    votoBranco = 0,
-    votoGanhador = 0,
-    porcentagem = 0,
-    porcentagemC1 = 0,
-    porcentagemC2 = 0,
-    porcentagemC3 = 0,
-    porcentagemBranco = 0,
-    porcentagemNulo = 0,
-    votoNulo = 0;
+    let voto;
+    let votosCandidato1 = 0;
+    let votosCandidato2 = 0;
+    let votosCandidato3 = 0;
+    let votosBrancos = 0;
+    let votosNulos = 0;
+    let totalVotos = 0;
+    let nomeGanhador;
+    let votosGanhador;
+    let ganhador = true;
 
-    let condicao = true
 
-    candidato1 = prompt("Digite o nome do Primeiro candidato: ");
-    candidato2 = prompt("Digite o nome do Segundo candidato: ");
-    candidato3 = prompt("Digite o nome do Terceiro candidato: ");
+    console.log ('Inicio Do Programa');
 
     do {
-        operacao = parseInt(prompt(`Digite o número correspondente ao seu voto:\n\n[1] ${candidato1}\n[2] ${candidato2}\n[3] ${candidato3}\n[5] Branco\n[8] Nulo`));
-        if(operacao === 1){
-            votoC1 += 1;
-            alert(`Candidato ${candidato1} votado com sucesso!`)
-        } else if(operacao === 2){
-            votoC2 += 1;
-            alert(`Candidato ${candidato2} votado com sucesso!`)
-        } else if(operacao === 3){
-            votoC3 += 1;
-            alert(`Candidato ${candidato3} votado com sucesso!`)
-        } else if(operacao === 5){
-            votoBranco += 1;
-            alert('Voto em branco registrado com sucesso!')
-        } else if(operacao === 8){
-            votoNulo += 1;
-            alert('Voto nulo registrado com sucesso!')
-        } else if(operacao === 150714) {
-            fim = prompt("Você tem certeza que deseja encerrar a operação? [S/N]")
-                if (fim === "S" || fim === "s") {
-                    alert("Encerrando a operação!");
-                    condicao = false;
-                }
+
+        console.clear();
+        console.log ('|1| Candidato 1');
+        console.log ('|2| Candidato 2');
+        console.log ('|3| Candidato 3');
+        console.log ('|5| Voto em branco');
+        console.log ('|8| Voto nulo');
+        console.log ('|0| Encerrar a votação');
+
+        voto = parseInt(prompt('Digite sua opção de voto:'));
+
+        
+
+        if (voto === 1) {
+            votosCandidato1 += 1;
+        } else if (voto === 2) {
+            votosCandidato2 += 1
+        } else if (voto === 3) {
+            votosCandidato3 += 1
+        } else if (voto === 5) {
+            votosBrancos += 1
+        } else if (voto === 8) {
+            votosNulos += 1
+        } else if (voto === 0) {
+            totalVotos --;
         } else {
-            alert("Operação inválida");
+            return;
         }
-    } while(condicao);
 
-    if(votoC1 > votoC2 && votoC1 > votoC3){
-        ganhador = candidato1;
-        votoGanhador = votoC1 + votoBranco;
-    } else if(votoC2 > votoC1 && votoC2 > votoC3){
-        ganhador = candidato2;
-        votoGanhador = votoC2 + votoBranco;
-    } else if(votoC3 > votoC1 && votoC3 > votoC2){
-        ganhador = candidato3;
-        votoGanhador = votoC3 + votoBranco;
-    } else {
-        alert(`Empate! Sem ganhadores.\nA quantidade de votos brancos e nulos foram de: ${porcentagemBranco.toFixed(2)}% e ${votoBranco} votos brancos no total e ${porcentagemNulo.toFixed(2)}% e ${votoNulo} votos nulos no total.`);
-    }
+    } while (voto !== 0);
 
-    porcentagemC1 = (votoC1 / (votoC1 + votoC2 + votoC3 + votoNulo + votoBranco)) * 100;
-    porcentagemC2 = (votoC2 / (votoC1 + votoC2 + votoC3 + votoNulo + votoBranco)) * 100;
-    porcentagemC3 = (votoC3 / (votoC1 + votoC2 + votoC3 + votoNulo + votoBranco)) * 100;
-    porcentagem = (votoGanhador / (votoC1 + votoC2 + votoC3 + votoNulo + votoBranco)) * 100;
-    porcentagemBranco = (votoBranco / (votoC1 + votoC2 + votoC3 + votoNulo + votoBranco)) * 100;
-    porcentagemNulo = (votoNulo / (votoC1 + votoC2 + votoC3 + votoNulo + votoBranco)) * 100;
-
-    if(ganhador !== ""){
-        alert(`O total de voto de todos os candidatos e seus percentuais foram:\n${candidato1}, ${votoC1} e ${porcentagemC1.toFixed(2)}%\n${candidato2}, ${votoC2} e ${porcentagemC2.toFixed(2)}%\n${candidato3}, ${votoC3} e ${porcentagemC3.toFixed(2)}%`);
-        alert(`A quantidade de votos brancos e nulos foram de:\n${porcentagemBranco.toFixed(2)}% e ${votoBranco} de votos brancos no total\n${porcentagemNulo.toFixed(2)}% e ${votoNulo} de votos nulos no total.`)
-        alert(`O ganhador foi ${ganhador} com ${porcentagem.toFixed(2)}% dos votos e ${votoGanhador} votos no total após acréscimo de votos brancos.`);
-    }
+    console.clear();
+    console.log('** BOLETIM DE URNA - RESULTADOS **');
+    console.log ('Total de votos do candidato1: ' + votosCandidato1 + ' votos (' + 
+    (votosCandidato1 / totalVotos * 100) + '%)');
     
+    console.log ('Total de votos do candidato1: ' + votosCandidato2 + ' votos (' + 
+    (votosCandidato2 / totalVotos * 100) + '%)');
+    
+    console.log ('Total de votos do candidato1: ' + votosCandidato3 + ' voto(s) (' + 
+    (votosCandidato3 / totalVotos * 100) + '%)');
+    
+    console.log ('Total de votos do candidato1: ' + votosBrancos + ' votos (' + 
+    (votosBrancos / totalVotos * 100) + '%)');
+    
+    console.log ('Total de votos do candidato1: ' + votosNulos + ' votos (' + 
+    (votosNulos / totalVotos * 100) + '%)');
+
+
+    if (votosCandidato1 > votosCandidato2 && votosCandidato1 > votosCandidato3) {
+        nomeGanhador = "Candidato 1";
+        votosGanhador = votosCandidato1 + votosBrancos;
+    } else if (votosCandidato2 > votosCandidato1 && votosCandidato2 > votosCandidato3) {
+        nomeGanhador = "Candidato 2";
+        votosGanhador = votosCandidato2 + votosBrancos;
+    } else if (votosCandidato3 > votosCandidato1 && votosCandidato3 > votosCandidato2) {
+        nomeGanhador = "Candidato 3";
+        votosGanhador = votosCandidato3 + votosBrancos;
+    } else {
+        ganhador = false;
+    }
+
+    if (ganhador === true) {
+        
+        console.log ('-----')
+        console.log ('O ganhador desta urna foi o candidato' + nomeGanhador + "com" + 
+        votosGanhador + 'voto(s) absoluto(s) + (' + (votosGanhador / totalVotos * 100) +
+        '%)');
+    } else {
+        console.log ('Não houve ganhador nesta urna (empate entre dois ou mais candidatos).');
+    }
+
 }
